cd11deae5b06579ef6800590e843ad3c64d33fb9
Yaroslav Klymko <t3hnar@gmail.com>
2019-09-09 16:08:03 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
 
   val scalatest            = "org.scalatest"       %% "scalatest"          % "3.0.8"
-  val `future-helper`      = "com.evolutiongaming" %% "future-helper"      % "1.0.5"
-  val `cassandra-launcher` = "com.evolutiongaming" %% "cassandra-launcher" % "0.0.2"
-  val scassandra           = "com.evolutiongaming" %% "scassandra"         % "1.1.1"
+  val `future-helper`      = "com.evolutiongaming" %% "future-helper"      % "1.0.6"
+  val `cassandra-launcher` = "com.evolutiongaming" %% "cassandra-launcher" % "0.0.3"
+  val scassandra           = "com.evolutiongaming" %% "scassandra"         % "1.1.2"
 
   object Logback {
     private val version = "1.2.3"
@@ -14,7 +14,7 @@ object Dependencies {
   }
 
   object Slf4j {
-    private val version = "1.7.26"
+    private val version = "1.7.28"
     val api                = "org.slf4j" % "slf4j-api"        % version
     val `log4j-over-slf4j` = "org.slf4j" % "log4j-over-slf4j" % version
   }
