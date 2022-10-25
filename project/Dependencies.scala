2f2ca4e5a171e1415a8de3ca02adf121779d1804
Scala Steward <scala_steward@virtuslab.com>
2022-10-25 15:40:34 +0000

    Update scassandra to 4.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scalatest            = "org.scalatest"       %% "scalatest"          % "3.2.3"
   val `future-helper`      = "com.evolutiongaming" %% "future-helper"      % "1.0.6"
   val `cassandra-launcher` = "com.evolutiongaming" %% "cassandra-launcher" % "0.0.4"
-  val scassandra           = "com.evolutiongaming" %% "scassandra"         % "4.0.1"
+  val scassandra           = "com.evolutiongaming" %% "scassandra"         % "4.1.0"
 
   object Logback {
     private val version = "1.2.3"
