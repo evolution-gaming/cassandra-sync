0e41f1d4c935e5727802f11dc227da38058751f4
Scala Steward <scala_steward@virtuslab.com>
2023-12-01 16:26:41 +0000

    Update logback-classic, logback-core to 1.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scassandra           = "com.evolutiongaming" %% "scassandra"         % "5.0.0"
 
   object Logback {
-    private val version = "1.2.3"
+    private val version = "1.2.13"
     val core    = "ch.qos.logback" % "logback-core"    % version
     val classic = "ch.qos.logback" % "logback-classic" % version
   }
