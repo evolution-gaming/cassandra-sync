c67d8302f3b10bc401a3ed374c0678c2809306a6
jmast <jmast@evolution.com>
2024-10-11 12:06:29 +0200

    bump library versions pin scala 3 to LTS

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scassandra           = "com.evolutiongaming" %% "scassandra"         % "5.2.1"
 
   object Logback {
-    private val version = "1.5.6"
+    private val version = "1.5.10"
     val core    = "ch.qos.logback" % "logback-core"    % version
     val classic = "ch.qos.logback" % "logback-classic" % version
   }
