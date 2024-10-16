5e682064f785eace318e29ce346ae86b26d921e2
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2024-10-16 10:48:42 +0300

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
 
   val scalatest            = "org.scalatest"       %% "scalatest"          % "3.2.19"
-  val `future-helper`      = "com.evolutiongaming" %% "future-helper"      % "1.0.7"
   val `testcontainers-cassandra` = "com.dimafeng"           %% "testcontainers-scala-cassandra"  % "0.41.4"
+  val `future-helper`      = "com.evolutiongaming" %% "future-helper"      % "1.0.7"
   val scassandra           = "com.evolutiongaming" %% "scassandra"         % "5.2.1"
 
   object Logback {
