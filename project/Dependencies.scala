9812f5e36a623b26159060ee6016e09bfa361c07
Mikhail Sokolov <msokolov@evolution.com>
2025-04-11 16:37:30 +0300

    Migrate to sbt-version-policy, update everything
    - migrated to v4 release action
    - updated ci action JVM version to 17
    - harmonized ci action with other Evo OSS projects
    - update Scala, plugins, and dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,13 +8,13 @@ object Dependencies {
   val scassandra           = "com.evolutiongaming" %% "scassandra"         % "5.3.0"
 
   object Logback {
-    private val version = "1.5.12"
+    private val version = "1.5.18"
     val core    = "ch.qos.logback" % "logback-core"    % version
     val classic = "ch.qos.logback" % "logback-classic" % version
   }
 
   object Slf4j {
-    private val version = "2.0.16"
+    private val version = "2.0.17"
     val api                = "org.slf4j" % "slf4j-api"        % version
     val `log4j-over-slf4j` = "org.slf4j" % "log4j-over-slf4j" % version
   }
